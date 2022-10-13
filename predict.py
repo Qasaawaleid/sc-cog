4722fb2a6d6fbc52e39506ae7f674dc6241bc4ef
M <mchaker@users.noreply.github.com>
2022-10-13 03:00:34 -0700

    predict: try another way to get 200 ok working

--- predict.py
+++ predict.py
@@ -95,8 +95,7 @@ class Predictor(BasePredictor):
         if any(output["nsfw_content_detected"]):
             return Output(http_status=451, text="NSFW content detected, please try a different prompt")
 
-        output_obj = Output()
-        output_obj.http_status = 200
+        output_obj = Output(http_status=200)
         for i, sample in enumerate(output["sample"]):
             output_path = f"/tmp/out-{i}.png"
             sample.save(output_path)
