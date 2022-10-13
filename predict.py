3812e1a95e2b61ab0cd4a7790f5ba395a83db625
M <mchaker@users.noreply.github.com>
2022-10-13 02:16:37 -0700

    predict: fix nsfw error handling

--- predict.py
+++ predict.py
@@ -88,7 +88,7 @@ class Predictor(BasePredictor):
             num_inference_steps=num_inference_steps,
         )
         if any(output["nsfw_content_detected"]):
-            raise Exception("NSFW content detected, please try a different prompt")
+            return ["nsfw", "NSFW content detected, please try a different prompt"]
 
         output_paths = []
         for i, sample in enumerate(output["sample"]):
