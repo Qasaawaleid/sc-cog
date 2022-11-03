b0e907ac7b1b01d5c2f21c7bdc7b48256c7cd4b0
yekta <ngyekta@gmail.com>
2022-11-04 01:58:52 +0300

    Self

--- predict.py
+++ predict.py
@@ -24,7 +24,7 @@ class Predictor(BasePredictor):
             cache_dir=MODEL_CACHE,
             local_files_only=True,
         ).to("cuda")
-        self.pipe.enable_xformers_memory_efficient_attention()
+        self.pipe.enable_xformers_memory_efficient_attention(self)
 
     @torch.inference_mode()
     @torch.cuda.amp.autocast()
