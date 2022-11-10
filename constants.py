2d2f718941916f5baa91b5c29817193ed16add76
yekta <ngyekta@gmail.com>
2022-11-10 16:29:27 +0300

    Locale to id

--- constants.py
+++ constants.py
@@ -1,1 +1,13 @@
 MODEL_CACHE = "diffusers-cache"
+LOCALE_TO_ID = {
+  "en": "eng_Latn",
+  "es": "spa_Latn",
+  "zh": "zho_Hans",
+  "hi": "hin_Deva",
+  "ru": "rus_Cyrl",
+  "ja": "jpn_Jpan",
+  "de": "deu_Latn",
+  "fr": "fra_Latn",
+  "tr": "tur_Latn",
+  "it": "ita_Latn",
+}
