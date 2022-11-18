879c2dc20a9a96c0b55ce342631434c0ab68429f
yekta <ngyekta@gmail.com>
2022-11-18 21:22:53 +0300

    Cleaned up real-esrgan stuff

--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import time
-import torch
 from diffusers import (
     PNDMScheduler,
     LMSDiscreteScheduler,
@@ -9,12 +8,7 @@ from diffusers import (
     EulerDiscreteScheduler,
     EulerAncestralDiscreteScheduler
 )
-from basicsr.archs.rrdbnet_arch import RRDBNet
-from basicsr.archs.srvgg_arch import SRVGGNetCompact
-from realesrgan.utils import RealESRGANer
-from gfpgan import GFPGANer
 from transformers import pipeline
-
 from constants import LANG_TO_ID, MODEL_CACHE
 from lingua import Language
 
@@ -28,7 +22,7 @@ def clean_folder(folder):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 def make_scheduler(name):
@@ -65,41 +59,11 @@ def make_scheduler(name):
         ),
     }[name]
 
-  
-def choose_model(self, scale, version, tile=0):
-    half = True if torch.cuda.is_available() else False
-    if version == 'General - RealESRGANplus':
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        model_path = 'weights/RealESRGAN_x4plus.pth'
-        self.upsampler = RealESRGANer(
-            scale=4, model_path=model_path, model=model, tile=tile, tile_pad=10, pre_pad=0, half=half)
-    elif version == 'General - v3':
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-        model_path = 'weights/realesr-general-x4v3.pth'
-        self.upsampler = RealESRGANer(
-            scale=4, model_path=model_path, model=model, tile=tile, tile_pad=10, pre_pad=0, half=half)
-    elif version == 'Anime - anime6B':
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
-        model_path = 'weights/RealESRGAN_x4plus_anime_6B.pth'
-        self.upsampler = RealESRGANer(
-            scale=4, model_path=model_path, model=model, tile=tile, tile_pad=10, pre_pad=0, half=half)
-    elif version == 'AnimeVideo - v3':
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
-        model_path = 'weights/realesr-animevideov3.pth'
-        self.upsampler = RealESRGANer(
-            scale=4, model_path=model_path, model=model, tile=tile, tile_pad=10, pre_pad=0, half=half)
-        
-    self.face_enhancer = GFPGANer(
-        model_path='weights/GFPGANv1.4.pth',
-        upscale=scale,
-        arch='clean',
-        channel_multiplier=2,
-        bg_upsampler=self.upsampler)
-
 
 eng_score_max = 0.9
 target_lang = Language.ENGLISH
 target_lang_id = LANG_TO_ID[target_lang.name]
+
 
 def translate_text(text, model, tokenizer, detector, label):
     if text == "":
--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ from diffusers import (
 )
 from PIL import Image
 from cog import BasePredictor, Input, Path
-from helpers import choose_model, make_scheduler, clean_folder, translate_text
+from helpers import make_scheduler, clean_folder, translate_text
 import cv2
 import tempfile
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
@@ -107,73 +107,16 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed.", default=None
         ),
-        img: Path = Input(description='Input', default=None),
-        version: str = Input(
-            description='RealESRGAN version. Please see [Readme] below for more descriptions.',
-            choices=['General - RealESRGANplus', 'General - v3', 'Anime - anime6B', 'AnimeVideo - v3'],
-            default='General - v3'),
-        scale: float = Input(description='Rescaling factor', default=2),
-        face_enhance: bool = Input(
-            description='Enhance faces with GFPGAN. Note that it does not work for anime images/vidoes.', default=False),
-        tile: int = Input(
-            description=
-            'Tile size. Default is 0, that is no tile. When encountering the out-of-GPU-memory issue, please specify it, e.g., 400 or 200.',
-            default=0),
         process_type: str = Input(
             description="Choose a process type, Can be 'generate' or 'upscale'.",
             choices=["generate", "upscale"],
             default="generate",
         )
     ) -> List[Path]:
+        """ out_path = Path(tempfile.mkdtemp()) / f'out.jpg'
+        cv2.imwrite(str(out_path), output, [int(cv2.IMWRITE_JPEG_QUALITY), 90]) """
         if process_type == 'upscale':
-            startTime = time.time()
-            if img is None:
-                raise Exception("Selected mode is upscale, an image is required")
-            if tile <= 100 or tile is None:
-                tile = 0
-            print(f'img: {img}. version: {version}. scale: {scale}. face_enhance: {face_enhance}. tile: {tile}.')
-            try:
-                extension = os.path.splitext(os.path.basename(str(img)))[1]
-                img = cv2.imread(str(img), cv2.IMREAD_UNCHANGED)
-                if len(img.shape) == 3 and img.shape[2] == 4:
-                    img_mode = 'RGBA'
-                elif len(img.shape) == 2:
-                    img_mode = None
-                    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-                else:
-                    img_mode = None
-
-                h, w = img.shape[0:2]
-                if h < 300:
-                    img = cv2.resize(img, (w * 2, h * 2), interpolation=cv2.INTER_LANCZOS4)
-
-                choose_model(self, scale, version, tile)
-
-                try:
-                    if face_enhance:
-                        _, _, output = self.face_enhancer.enhance(
-                            img, has_aligned=False, only_center_face=False, paste_back=True)
-                    else:
-                        output, _ = self.upsampler.enhance(img, outscale=scale)
-                except RuntimeError as error:
-                    print('Error', error)
-                    print('If you encounter CUDA out of memory, try to set "tile" to a smaller size, e.g., 400.')
-
-                if img_mode == 'RGBA':  # RGBA images should be saved in png format
-                    extension = 'png'
-                # save_path = f'output/out.{extension}'
-                # cv2.imwrite(save_path, output)
-                
-                # out_path = Path(tempfile.mkdtemp()) / f'out.{extension}'
-                # force jpg for smaller size
-                out_path = Path(tempfile.mkdtemp()) / f'out.jpg'
-                cv2.imwrite(str(out_path), output, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-            except Exception as error:
-                print('global exception: ', error)
-            finally:
-                clean_folder('output')
-            output_paths = []
-            output_paths.append(out_path)
+            
             endTime = time.time()
             print(f"-- Upscaled in: {endTime - startTime} sec. --")
             return output_paths
