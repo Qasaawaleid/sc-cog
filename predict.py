7dbf78f22d8c43b9e962fcbad14e81757c549f88
yekta <ngyekta@gmail.com>
2022-11-18 22:28:24 +0300

    Swinir stuff

--- predict.py
+++ predict.py
@@ -14,9 +14,11 @@ from helpers import make_scheduler, clean_folder, translate_text
 import cv2
 import tempfile
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from collections import OrderedDict
+import shutil
+import numpy as np
 
-
-from constants import MODEL_CACHE, TRANSLATOR_MODEL_CACHE, TRANSLATOR_TOKENIZER_CACHE
+from constants import MODEL_CACHE, MODELS_SWINIR, TASKS_SWINIR, TRANSLATOR_MODEL_CACHE, TRANSLATOR_TOKENIZER_CACHE
 from lingua import LanguageDetectorBuilder
 
 class Predictor(BasePredictor):
@@ -107,6 +109,29 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed.", default=None
         ),
+        image_u: Path = Input(
+            description="Input image for the upscaler (Swinir).", default=None
+        ),
+        task_u: str = Input(
+            default="Real-World Image Super-Resolution",
+            choices=[
+                'Real-World Image Super-Resolution',
+                'Grayscale Image Denoising',
+                'Color Image Denoising',
+                'JPEG Compression Artifact Reduction'
+            ],
+            description="Task type for the upscaler (Swinir).",
+        ),
+        noise_u: int = Input(
+            description='Noise level, activated for Grayscale Image Denoising and Color Image Denoising. It is for the upscaler (Swinir). Leave it as default or arbitrary if other tasks are selected.',
+            choices=[15, 25, 50],
+            default=15,
+        ),
+        jpeg_u: int = Input(
+            description='Scale factor, activated for JPEG Compression Artifact Reduction. It is for the upscaler (Swinir). Leave it as default or arbitrary if other tasks are selected.',
+            choices=[10, 20, 30, 40],
+            default=40,
+        ),
         process_type: str = Input(
             description="Choose a process type, Can be 'generate' or 'upscale'.",
             choices=["generate", "upscale"],
@@ -116,10 +141,74 @@ class Predictor(BasePredictor):
         """ out_path = Path(tempfile.mkdtemp()) / f'out.jpg'
         cv2.imwrite(str(out_path), output, [int(cv2.IMWRITE_JPEG_QUALITY), 90]) """
         if process_type == 'upscale':
+            task = TASKS_SWINIR[task_u]
+            scale = 4
+            model_path = ""
+            folder_lq = None
+            folder_gt = None
+            if task == 'real_sr':
+                model_path = MODELS_SWINIR[task][4]
+            elif task in ['gray_dn', 'color_dn']:
+                model_path = MODELS_SWINIR[task][noise_u]
+            else:
+                model_path = MODELS_SWINIR[task][jpeg_u]
             
+            try:
+                # set input folder
+                input_dir = 'input_cog_temp'
+                os.makedirs(input_dir, exist_ok=True)
+                input_path = os.path.join(input_dir, os.path.basename(image_u))
+                shutil.copy(str(image_u), input_path)
+                if task == 'real_sr':
+                    folder_lq = input_dir
+                else:
+                    folder_gt = input_dir
+
+                model = define_model(self.args)
+                model.eval()
+                model = model.to("cuda")
+
+                # setup folder and path
+                folder, save_dir, border, window_size = setup(self.args)
+                os.makedirs(save_dir, exist_ok=True)
+                test_results = OrderedDict()
+                test_results['psnr'] = []
+                test_results['ssim'] = []
+                test_results['psnr_y'] = []
+                test_results['ssim_y'] = []
+                test_results['psnr_b'] = []
+                # psnr, ssim, psnr_y, ssim_y, psnr_b = 0, 0, 0, 0, 0
+                out_path = Path(tempfile.mkdtemp()) / "out.png"
+
+                for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, '*')))):
+                    # read image
+                    imgname, img_lq, img_gt = get_image_pair(self.args, path)  # image to HWC-BGR, float32
+                    img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]],
+                                        (2, 0, 1))  # HCW-BGR to CHW-RGB
+                    img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(self.device)  # CHW-RGB to NCHW-RGB
+
+                    # inference
+                    with torch.no_grad():
+                        # pad input image to be a multiple of window_size
+                        _, _, h_old, w_old = img_lq.size()
+                        h_pad = (h_old // window_size + 1) * window_size - h_old
+                        w_pad = (w_old // window_size + 1) * window_size - w_old
+                        img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[:, :, :h_old + h_pad, :]
+                        img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[:, :, :, :w_old + w_pad]
+                        output = model(img_lq)
+                        output = output[..., :h_old * self.args.scale, :w_old * self.args.scale]
+
+                    # save image
+                    output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+                    if output.ndim == 3:
+                        output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))  # CHW-RGB to HCW-BGR
+                    output = (output * 255.0).round().astype(np.uint8)  # float32 to uint8
+                    cv2.imwrite(str(out_path), output)
+            finally:
+                clean_folder(input_dir)
             endTime = time.time()
             print(f"-- Upscaled in: {endTime - startTime} sec. --")
-            return output_paths
+            return out_path
         else:
             """Run a single prediction on the model"""
             startTime = time.time()
