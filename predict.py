ffb90aed7c83b738aaf096a1784ffd50f57590c4
yekta <ngyekta@gmail.com>
2022-11-19 22:27:05 +0300

    Swinir args

--- predict.py
+++ predict.py
@@ -181,17 +181,17 @@ class Predictor(BasePredictor):
                 os.makedirs(input_dir, exist_ok=True)
                 input_path = os.path.join(input_dir, os.path.basename(image_u))
                 shutil.copy(str(image_u), input_path)
-                if self.args.task == 'real_sr':
-                    self.args.folder_lq = input_dir
+                if self.swinir_args.task == 'real_sr':
+                    self.swinir_args.folder_lq = input_dir
                 else:
-                    self.args.folder_gt = input_dir
+                    self.swinir_args.folder_gt = input_dir
 
-                model = define_model_swinir(self.args)
+                model = define_model_swinir(self.swinir_args)
                 model.eval()
                 model = model.to(self.device)
 
                 # setup folder and path
-                folder, save_dir, border, window_size = setup_swinir(self.args)
+                folder, save_dir, border, window_size = setup_swinir(self.swinir_args)
                 os.makedirs(save_dir, exist_ok=True)
                 test_results = OrderedDict()
                 test_results['psnr'] = []
@@ -204,7 +204,7 @@ class Predictor(BasePredictor):
 
                 for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, '*')))):
                     # read image
-                    imgname, img_lq, img_gt = get_image_pair_swinir(self.args, path)  # image to HWC-BGR, float32
+                    imgname, img_lq, img_gt = get_image_pair_swinir(self.swinir_args, path)  # image to HWC-BGR, float32
                     img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]],
                                         (2, 0, 1))  # HCW-BGR to CHW-RGB
                     img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(self.device)  # CHW-RGB to NCHW-RGB
@@ -218,7 +218,7 @@ class Predictor(BasePredictor):
                         img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[:, :, :h_old + h_pad, :]
                         img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[:, :, :, :w_old + w_pad]
                         output = model(img_lq)
-                        output = output[..., :h_old * self.args.scale, :w_old * self.args.scale]
+                        output = output[..., :h_old * self.swinir_args.scale, :w_old * self.swinir_args.scale]
 
                     # save image
                     output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
