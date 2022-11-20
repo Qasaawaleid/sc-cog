be2e664b8520308adc36602f78a72b256b551824
yekta <ngyekta@gmail.com>
2022-11-20 15:13:30 +0300

    Get args swinir

--- models/swinir/helpers.py
+++ models/swinir/helpers.py
@@ -1,4 +1,5 @@
 from .network_swinir import SwinIR as net
+from .constants import MODELS_SWINIR
 import torch
 import cv2
 import numpy as np
@@ -153,3 +154,23 @@ def get_image_pair(args, path):
         img_lq = img_lq.astype(np.float32)/ 255.
 
     return imgname, img_lq, img_gt
+
+
+def get_args_swinir():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='real_sr', help='classical_sr, lightweight_sr, real_sr, '
+                                                                    'gray_dn, color_dn, jpeg_car')
+    parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8')  # 1 for dn and jpeg car
+    parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
+    parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
+    parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
+                                                                                'Just used to differentiate two different settings in Table 2 of the paper. '
+                                                                                'Images are NOT tested patch by patch.')
+    parser.add_argument('--large_model', action='store_true',
+                        help='use large model, only provided for real image sr')
+    parser.add_argument('--model_path', type=str,
+                        default=MODELS_SWINIR['real_sr']['large'])
+    parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
+    parser.add_argument('--folder_gt', type=str, default=None, help='input ground-truth test image folder')
+    
+    return parser.parse_args('')
--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-import argparse
 import time
 from typing import List
 
@@ -13,13 +12,15 @@ from cog import BasePredictor, Input, Path
 from models.stable_diffusion.generate import generate
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+from models.swinir.helpers import get_args_swinir
+
 from .models.stable_diffusion.constants import SD_MODEL_CACHE
-from .models.swinir.constants import MODELS_SWINIR
 from .models.nllb.constants import TRANSLATOR_MODEL_CACHE, TRANSLATOR_TOKENIZER_CACHE 
 from .models.nllb.translate import translate_text
+from .models.swinir.upscale import upscale
+
 from lingua import LanguageDetectorBuilder
 
-from models.swinir.upscale import upscale
 
 class Predictor(BasePredictor):
     def setup(self):
@@ -61,23 +62,7 @@ class Predictor(BasePredictor):
         self.translate_tokenizer = AutoTokenizer.from_pretrained(translate_model_name, cache_dir=TRANSLATOR_TOKENIZER_CACHE)
         self.translate_model = AutoModelForSeq2SeqLM.from_pretrained(translate_model_name, cache_dir=TRANSLATOR_MODEL_CACHE).to("cuda")
         
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--task', type=str, default='real_sr', help='classical_sr, lightweight_sr, real_sr, '
-                                                                        'gray_dn, color_dn, jpeg_car')
-        parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8')  # 1 for dn and jpeg car
-        parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
-        parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
-        parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
-                                                                                 'Just used to differentiate two different settings in Table 2 of the paper. '
-                                                                                 'Images are NOT tested patch by patch.')
-        parser.add_argument('--large_model', action='store_true',
-                            help='use large model, only provided for real image sr')
-        parser.add_argument('--model_path', type=str,
-                            default=MODELS_SWINIR['real_sr']['large'])
-        parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
-        parser.add_argument('--folder_gt', type=str, default=None, help='input ground-truth test image folder')
-        
-        self.swinir_args = parser.parse_args('')
+        self.swinir_args = get_args_swinir()
         self.device = torch.device('cuda')
 
     @torch.inference_mode()
