5c687c7dafb0f2f665dcbf2c5c7c4eacb03c5ac7
yekta <ngyekta@gmail.com>
2022-11-23 04:06:53 +0300

    Mat

--- models/stable_diffusion/generate.py
+++ models/stable_diffusion/generate.py
@@ -88,8 +88,8 @@ def generate(
       output_path_png = f"/tmp/out-{i}.png"
       output_path_jpg = f"/tmp/out-{i}.jpg"
       sample.save(output_path_png)
-      png = cv2.imread(output_path_png)
-      cv2.imwrite(output_path_jpg, png, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+      pngMat = cv2.imread(output_path_png)
+      cv2.imwrite(output_path_jpg, pngMat, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
       output_paths.append(Path(output_path_jpg))
       end = time.time()
       print(f"Saved image {i} in: {end - start} sec.")
