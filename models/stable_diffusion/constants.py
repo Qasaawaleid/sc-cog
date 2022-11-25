e7c830e937f6455bc2474e9bf256d0bf9c6c18cb
yekta <ngyekta@gmail.com>
2022-11-25 21:17:51 +0300

    model id fixed

--- models/stable_diffusion/constants.py
+++ models/stable_diffusion/constants.py
@@ -1,4 +1,4 @@
 SD_MODEL_ID= "runwayml/stable-diffusion-v1-5"
 SD_MODEL_ID_OJ = "prompthero/openjourney"
-SD_MODEL_ID_WF = "waifu-diffusion"
+SD_MODEL_ID_WF = "hakurei/waifu-diffusion"
 SD_MODEL_CACHE = "diffusers-cache"
