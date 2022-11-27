aaf65c8b2d4166d0405d41db9cd38e8efe199c38
yekta <ngyekta@gmail.com>
2022-11-27 20:16:24 +0300

    Dictionary name fix

--- models/stable_diffusion/helpers.py
+++ models/stable_diffusion/helpers.py
@@ -5,37 +5,37 @@ from diffusers import (
     EulerDiscreteScheduler,
     EulerAncestralDiscreteScheduler
 )
-from .constants import SD_MODEL_CACHE, SD_MODEL_TO_MODEL_ID
+from .constants import SD_MODEL_CACHE, SD_MODEL_TO_ID
 
 
 def make_scheduler(name, model):
     return {
         "PNDM": PNDMScheduler.from_config(
-            SD_MODEL_TO_MODEL_ID[model],
+            SD_MODEL_TO_ID[model],
             cache_dir=SD_MODEL_CACHE, 
             local_files_only=True, 
             subfolder="scheduler"
         ),
         "K_LMS": LMSDiscreteScheduler.from_config(
-            SD_MODEL_TO_MODEL_ID[model],
+            SD_MODEL_TO_ID[model],
             cache_dir=SD_MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler"
         ),
         "DDIM": DDIMScheduler.from_config(
-            SD_MODEL_TO_MODEL_ID[model],
+            SD_MODEL_TO_ID[model],
             cache_dir=SD_MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler"
         ),
         "K_EULER": EulerDiscreteScheduler.from_config(
-            SD_MODEL_TO_MODEL_ID[model],
+            SD_MODEL_TO_ID[model],
             cache_dir=SD_MODEL_CACHE, 
             local_files_only=True, 
             subfolder="scheduler"
         ),
         "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(
-            SD_MODEL_TO_MODEL_ID[model],
+            SD_MODEL_TO_ID[model],
             cache_dir=SD_MODEL_CACHE, 
             local_files_only=True,
             subfolder="scheduler"
