10a379b7a0b47dc8c7d5ae48b22bf5464a49bba7
yekta <ngyekta@gmail.com>
2022-12-05 17:29:17 +0300

    Target lang

--- models/nllb/translate.py
+++ models/nllb/translate.py
@@ -3,7 +3,7 @@ import time
 from transformers import pipeline
 from .constants import LANG_TO_ID
 
-eng_score_max = 0.8
+target_lang_score_max = 0.8
 target_lang = Language.ENGLISH
 target_lang_id = LANG_TO_ID[target_lang.name]
 
@@ -16,7 +16,7 @@ def translate_text(text, model, tokenizer, detector, label):
     text_lang_id = target_lang_id
     
     confidence_values = detector.compute_language_confidence_values(text)
-    eng_value = None
+    target_lang_score = None
     detected_lang = None
     detected_lang_score = None
     
@@ -25,16 +25,16 @@ def translate_text(text, model, tokenizer, detector, label):
         if index == 0:
             detected_lang = curr[0]
             detected_lang_score = curr[1]
-        if curr[0] == Language.ENGLISH:
-            eng_value = curr[1]
+        if curr[0] == target_lang_id:
+            target_lang_score = curr[1]
             
-    if detected_lang is not None and detected_lang != target_lang and (eng_value is None or eng_value < eng_score_max) and LANG_TO_ID.get(detected_lang.name) is not None:
+    if detected_lang is not None and detected_lang != target_lang and (target_lang_score is None or target_lang_score < target_lang_score_max) and LANG_TO_ID.get(detected_lang.name) is not None:
         text_lang_id = LANG_TO_ID[detected_lang.name]
     
     if detected_lang is not None:
         print(f'-- {label} - Guessed text language: "{detected_lang.name}". Score: {detected_lang_score} --')
-    if eng_value is not None:
-        print(f'-- {label} - English score: {eng_value} --')
+        if target_lang_score is not None and detected_lang != target_lang:
+            print(f'-- {label} - Target lang score: {target_lang_score} --')
     print(f'-- {label} - Selected text language id: "{text_lang_id}" --')
     
     if text_lang_id != target_lang_id:
