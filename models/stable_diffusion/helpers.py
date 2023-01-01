33200ba082a992da81882f82a154811cbe10c087
yekta <40706601+yekta@users.noreply.github.com>
2023-01-01 22:30:27 +0300

    Helpers fix

--- models/stable_diffusion/helpers.py
+++ models/stable_diffusion/helpers.py
@@ -5,41 +5,41 @@ from diffusers import (
     EulerDiscreteScheduler,
     EulerAncestralDiscreteScheduler
 )
-from .constants import SD_MODEL_CACHE, SD_MODEL_TO_ID
+from .constants import SD_MODEL_CACHE, SD_MODELS
 
 
 def make_scheduler(name, model, revision):
     return {
         "PNDM": PNDMScheduler.from_config(
-            SD_MODEL_TO_ID[model],
+            SD_MODELS[model]["id"],
             cache_dir=SD_MODEL_CACHE,
             local_files_only=True, 
             subfolder="scheduler",
             revision=revision or "main"
         ),
         "K_LMS": LMSDiscreteScheduler.from_config(
-            SD_MODEL_TO_ID[model],
+            SD_MODELS[model]["id"],
             cache_dir=SD_MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
             revision=revision or "main"
         ),
         "DDIM": DDIMScheduler.from_config(
-            SD_MODEL_TO_ID[model],
+            SD_MODELS[model]["id"],
             cache_dir=SD_MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
             revision=revision or "main"
         ),
         "K_EULER": EulerDiscreteScheduler.from_config(
-            SD_MODEL_TO_ID[model],
+            SD_MODELS[model]["id"],
             cache_dir=SD_MODEL_CACHE, 
             local_files_only=True, 
             subfolder="scheduler",
             revision=revision or "main"
         ),
         "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(
-            SD_MODEL_TO_ID[model],
+            SD_MODELS[model]["id"],
             cache_dir=SD_MODEL_CACHE, 
             local_files_only=True,
             subfolder="scheduler",
