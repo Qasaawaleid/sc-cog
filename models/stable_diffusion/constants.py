9e191415d67356fdc424ec4d7f98e4a1367cac35
yekta <ngyekta@gmail.com>
2023-01-03 16:00:49 +0300

    Download script downloads everything

--- models/stable_diffusion/constants.py
+++ models/stable_diffusion/constants.py
@@ -12,62 +12,58 @@ from diffusers import (
 )
 
 SD_MODEL_CACHE = "diffusers-cache"
+SD_MODELS_FULL = {
+    "Stable Diffusion v1.5": {
+        "id": "runwayml/stable-diffusion-v1-5",
+        "revision": "fp16",
+        "torch_dtype": torch.float16
+    },
+    "Openjourney": {
+        "id": "prompthero/openjourney",
+        "prompt_prefix": "mdjrny-v4 style",
+        "torch_dtype": torch.float16
+    },
+    "Redshift Diffusion": {
+        "id": "nitrosocke/redshift-diffusion",
+        "prompt_prefix": "redshift style",
+        "torch_dtype": torch.float16
+    },
+    "Arcane Diffusion": {
+        "id": "nitrosocke/Arcane-Diffusion",
+        "prompt_prefix": "arcane style",
+        "torch_dtype": torch.float16
+    },
+    "Ghibli Diffusion": {
+        "id": "nitrosocke/Ghibli-Diffusion",
+        "prompt_prefix": "ghibli style",
+        "torch_dtype": torch.float16
+    },
+    "Mo-Di Diffusion": {
+        "id": "nitrosocke/mo-di-diffusion",
+        "prompt_prefix": "modern disney style",
+        "torch_dtype": torch.float16
+    },
+    "Waifu Diffusion v1.4": {
+        "id": "hakurei/waifu-diffusion",
+        "prompt_prefix": "masterpiece, best quality, high quality",
+        "negative_prompt_prefix": "worst quality, low quality, deleted, nsfw",
+        "torch_dtype": torch.float32
+    },
+    "22h Diffusion v0.1": {
+        "id": "22h/vintedois-diffusion-v0-1",
+        "prompt_prefix": "estilovintedois",
+        "torch_dtype": torch.float16
+    }
+}
 
 SD_MODELS = {}
 models_from_env = os.environ.get(
-    "MODELS", "Stable Diffusion v1.5,Openjourney,Redshift Diffusion,Arcane Diffusion,Ghibli Diffusion,Mo-Di Diffusion,Waifu Diffusion v1.4,22h Diffusion v0.1"
+    "MODELS", "Stable Diffusion v1.5,Openjourney"
 )
 models_from_env_list = models_from_env.split(",")
 for model_env in models_from_env_list:
-    if model_env == "Stable Diffusion v1.5":
-        SD_MODELS[model_env] = {
-            "id": "runwayml/stable-diffusion-v1-5",
-            "revision": "fp16",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Openjourney":
-        SD_MODELS[model_env] = {
-            "id": "prompthero/openjourney",
-            "prompt_prefix": "mdjrny-v4 style",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Redshift Diffusion":
-        SD_MODELS[model_env] = {
-            "id": "nitrosocke/redshift-diffusion",
-            "prompt_prefix": "redshift style",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Arcane Diffusion":
-        SD_MODELS[model_env] = {
-            "id": "nitrosocke/Arcane-Diffusion",
-            "prompt_prefix": "arcane style",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Ghibli Diffusion":
-        SD_MODELS[model_env] = {
-            "id": "nitrosocke/Ghibli-Diffusion",
-            "prompt_prefix": "ghibli style",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Mo-Di Diffusion":
-        SD_MODELS[model_env] = {
-            "id": "nitrosocke/mo-di-diffusion",
-            "prompt_prefix": "modern disney style",
-            "torch_dtype": torch.float16
-        }
-    elif model_env == "Waifu Diffusion v1.4":
-        SD_MODELS[model_env] = {
-            "id": "hakurei/waifu-diffusion",
-            "prompt_prefix": "masterpiece, best quality, high quality",
-            "negative_prompt_prefix": "worst quality, low quality, deleted, nsfw",
-            "torch_dtype": torch.float32
-        }
-    elif model_env == "22h Diffusion v0.1":
-        SD_MODELS[model_env] = {
-            "id": "22h/vintedois-diffusion-v0-1",
-            "prompt_prefix": "estilovintedois",
-            "torch_dtype": torch.float16
-        }
+    if model_env in SD_MODELS_FULL:
+        SD_MODELS[model_env] = SD_MODELS_FULL[model_env]
 
 SD_MODEL_CHOICES = list(SD_MODELS.keys())
 SD_MODEL_DEFAULT_KEY = SD_MODEL_CHOICES[0]
