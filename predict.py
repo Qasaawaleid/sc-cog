5bfa0a62b04331ca16d4f6de6d3cb06ac865fc10
yekta <ngyekta@gmail.com>
2023-01-19 04:01:43 +0300

    Add back autocast

--- predict.py
+++ predict.py
@@ -60,6 +60,7 @@ class Predictor(BasePredictor):
         print("✅ Setup is done!")
 
     @torch.inference_mode()
+    @torch.cuda.amp.autocast()
     def predict(
         self,
         prompt: str = Input(description="Input prompt.", default=""),
