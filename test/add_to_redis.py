7db93992aff8964905fc57c5252c55f0db2e875f
yekta <ngyekta@gmail.com>
2023-01-22 18:05:51 +0300

    Test scripts

---
+++ test/add_to_redis.py
@@ -0,0 +1,87 @@
+from json import JSONEncoder
+import itertools
+import uuid
+import redis
+import argparse
+
+encoder = JSONEncoder()
+
+
+def add_combinations_to_queue(
+    models,
+    widths,
+    heights,
+    steps_list,
+    outputs,
+    redis_connection_string,
+    redis_queue_name,
+    webhook_url
+):
+    # Connect to Redis
+    r = redis.from_url(redis_connection_string)
+
+    # Create the queue if it doesn't exist
+    if not r.exists(redis_queue_name):
+        r.execute_command(
+            'XGROUP', 'CREATE', redis_queue_name,
+            redis_queue_name, '$', 'MKSTREAM'
+        )
+        print(f'Created queue "{redis_queue_name}"')
+    else:
+        print(f'Queue "{redis_queue_name}" already exists')
+
+    # Create a list of all possible combinations of the input parameters
+    combinations = list(itertools.product(
+        models, widths, heights, steps_list, outputs
+    ))
+
+    # Iterate through the combinations and add them to the Redis queue
+    for model, width, height, steps, output in combinations:
+        input_id = f'test-{str(uuid.uuid4())}'
+        val = {
+            'webhook_events_filter': ["start", "completed"],
+            'webhook': str(webhook_url),
+            'input': {
+                'id': str(input_id),
+                'prompt': 'Portrait of a cat by Van Gogh',
+                'width': str(width),
+                'height': str(height),
+                'num_inference_steps': str(steps),
+                'model': str(model),
+                'num_outputs': str(output),
+            },
+            'response_queue': 'output-queue'
+        }
+        # convert value to string
+        val = encoder.encode(val)
+        data = {
+            'value': val
+        }
+        r.xadd(redis_queue_name, data)
+        print(
+            f'Added {model}, {width}, {height}, {steps}, {output} to queue'
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--models', nargs='+',
+                        required=True, help='List of models')
+    parser.add_argument('-w', '--widths', nargs='+',
+                        required=True, help='List of widths')
+    parser.add_argument('-e', '--heights', nargs='+',
+                        required=True, help='List of heights')
+    parser.add_argument('-s', '--steps-list', nargs='+',
+                        required=True, help='List of steps')
+    parser.add_argument('-o', '--outputs', nargs='+',
+                        required=True, help='List of outputs')
+    parser.add_argument('--redis-connection-string',
+                        required=True, help='Redis connection string')
+    parser.add_argument('--redis-queue-name', required=True,
+                        help='Redis queue name')
+    parser.add_argument('--webhook-url', required=True, help='Webhook url')
+    args = parser.parse_args()
+    add_combinations_to_queue(
+        args.models, args.widths, args.heights, args.steps_list, args.outputs,
+        args.redis_connection_string, args.redis_queue_name, args.webhook_url
+    )
---
+++ test/parse.py
@@ -0,0 +1,37 @@
+import re
+import csv
+from operator import itemgetter
+
+
+def parse_logs(file_name):
+    data = []
+    with open(file_name, "r") as f:
+        log_block = f.read()
+        log_lines = log_block.strip().split("\n")
+        for line in log_lines:
+            match = re.search(
+                r"🖥️ Generated in (\d+) ms - Model: (.+?) - Width: (.+?) - Height: (.+?) - Steps: (.+?) - Outputs: (.+?) 🖥️", line)
+            if match:
+                data.append(
+                    [
+                        match.group(3),
+                        match.group(4),
+                        match.group(5),
+                        match.group(6),
+                        match.group(2),
+                        match.group(1),
+                    ]
+                )
+    return data
+
+
+def write_to_csv(data):
+    with open("test/logs.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        for row in data:
+            writer.writerow(row)
+
+
+data = parse_logs("test/logs.txt")
+data.sort(key=itemgetter(0, 2, 3))
+write_to_csv(data)
