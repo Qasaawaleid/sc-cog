33f01a9cdcd85cda03a24b5f068890e05395d18a
yekta <ngyekta@gmail.com>
2023-01-27 22:33:28 +0300

    Build

--- predict.py
+++ predict.py
@@ -35,6 +35,7 @@ class Predictor(BasePredictor):
         # Login to Hugging Face
         login(token=os.environ.get("HUGGINGFACE_TOKEN"))
 
+        #
         # Download all models concurrently
         with ThreadPoolExecutor(5) as executor:
             tasks = []
