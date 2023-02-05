f7b6d1ca0471f59c56ed69a995977971fecbbbf3
yekta <ngyekta@gmail.com>
2023-02-05 18:01:31 +0300

    Update upscale

--- models/swinir/upscale.py
+++ models/swinir/upscale.py
@@ -13,9 +13,10 @@ from cog import Path
 from .constants import MODELS_SWINIR, TASKS_SWINIR
 from .helpers import define_model, get_image_pair, setup, get_args_swinir
 
-device = torch.device('cuda')
+device = torch.device("cuda")
 
 
+@torch.cuda.amp.autocast()
 def upscale(image):
     if image is None:
         raise ValueError("Image is required for the upscaler.")
@@ -28,7 +29,7 @@ def upscale(image):
 
     try:
         # set input folder
-        input_dir = 'input_cog_temp'
+        input_dir = "input_cog_temp"
         os.makedirs(input_dir, exist_ok=True)
         input_path = os.path.join(input_dir, os.path.basename(image))
         shutil.copy(str(image), input_path)
@@ -43,22 +44,25 @@ def upscale(image):
         folder, save_dir, border, window_size = setup(args)
         os.makedirs(save_dir, exist_ok=True)
         test_results = OrderedDict()
-        test_results['psnr'] = []
-        test_results['ssim'] = []
-        test_results['psnr_y'] = []
-        test_results['ssim_y'] = []
-        test_results['psnr_b'] = []
+        test_results["psnr"] = []
+        test_results["ssim"] = []
+        test_results["psnr_y"] = []
+        test_results["ssim_y"] = []
+        test_results["psnr_b"] = []
         # psnr, ssim, psnr_y, ssim_y, psnr_b = 0, 0, 0, 0, 0
 
         out_path = Path(tempfile.mkdtemp()) / "out.png"
-        for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, '*')))):
+        for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, "*")))):
             # read image
             imgname, img_lq, img_gt = get_image_pair(
-                args, path)  # image to HWC-BGR, float32
-            img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]],
-                                  (2, 0, 1))  # HCW-BGR to CHW-RGB
-            img_lq = torch.from_numpy(img_lq).float().unsqueeze(
-                0).to(device)  # CHW-RGB to NCHW-RGB
+                args, path
+            )  # image to HWC-BGR, float32
+            img_lq = np.transpose(
+                img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]], (2, 0, 1)
+            )  # HCW-BGR to CHW-RGB
+            img_lq = (
+                torch.from_numpy(img_lq).float().unsqueeze(0).to(device)
+            )  # CHW-RGB to NCHW-RGB
 
             # inference
             with torch.no_grad():
@@ -67,11 +71,13 @@ def upscale(image):
                 h_pad = (h_old // window_size + 1) * window_size - h_old
                 w_pad = (w_old // window_size + 1) * window_size - w_old
                 img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[
-                    :, :, :h_old + h_pad, :]
+                    :, :, : h_old + h_pad, :
+                ]
                 img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[
-                    :, :, :, :w_old + w_pad]
+                    :, :, :, : w_old + w_pad
+                ]
                 output = model(img_lq)
-                output = output[..., :h_old * args.scale, :w_old * args.scale]
+                output = output[..., : h_old * args.scale, : w_old * args.scale]
 
             # save image
             output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
